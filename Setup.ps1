779bbd26c6739c15d0d44d9b5275cff8b1685c02
Brandon <brandon.weir25@protonmail.com>
2021-02-28 17:40:13 -0600

    Scripted a fix for operating systems newer than Windows Server 2016 and fixed some misconfigured GPO settings

--- Setup.ps1
+++ Setup.ps1
@@ -29,8 +29,23 @@ wecutil.exe qc -quiet
 # Creates the WEFC subscription(s)
 $subscriptions = Get-ChildItem -Path "$env:SystemDrive\WEFC\subscriptions\" -File -Filter "*.xml"
 foreach ($item in $subscriptions) {
-    wecutil.exe cs $item.FullName
+	wecutil.exe cs $item.FullName
 }
 
 # Sets the Windows Event Collector Service startup type to automatic
-Set-Service -Name WECSVC -StartupType "Automatic"
+Set-Service -Name 'Wecsvc' -StartupType "Automatic"
+
+# Fix for Windows Server 2019+ and Windows 10 1703+ with more than 3.5GB of RAM
+# https://docs.microsoft.com/en-us/troubleshoot/windows-server/admin-development/events-not-forwarded-by-windows-server-collector#symptoms
+# https://docs.microsoft.com/en-us/windows/application-management/svchost-service-refactoring
+
+$OperatingSystem = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -ExpandProperty Caption
+$MemoryCapacity = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1GB
+If(($OperatingSystem -notlike "*Windows Server 2016*") -and 
+	($MemoryCapacity -gt "3.5")) {
+	Start-Process cmd.exe -ArgumentList "/c netsh http delete urlacl url=http://+:5985/wsman/"
+	Start-Process cmd.exe -ArgumentList "/c netsh http add urlacl url=http://+:5985/wsman/ sddl=D:(A;;GX;;;S-1-5-80-569256582-2953403351-2909559716-1301513147-412116970)(A;;GX;;;S-1-5-80-4059739203-877974739-1245631912-527174227-2996563517)"
+	Start-Process cmd.exe -ArgumentList "/c netsh http delete urlacl url=https://+:5986/wsman/"
+	Start-Process cmd.exe -ArgumentList "/c netsh http add urlacl url=https://+:5986/wsman/ sddl=D:(A;;GX;;;S-1-5-80-569256582-2953403351-2909559716-1301513147-412116970)(A;;GX;;;S-1-5-80-4059739203-877974739-1245631912-527174227-2996563517)"
+	Restart-Service -Name 'Wecsvc'
+}
